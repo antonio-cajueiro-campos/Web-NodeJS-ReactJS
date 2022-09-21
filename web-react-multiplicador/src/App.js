ec59fdf82c549e622f66d6f7a8bf04798cd9fbb6
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-09-21 02:43:11 -0300

    app finalizado

--- web-react-multiplicador/src/App.js
+++ web-react-multiplicador/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { React, useState } from 'react'
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [number2, setNumber2] = useState(0);
+  const [result, setResult] = useState(0);
+
+  
+  const calc = () => {
+    setResult(_ => number1 * number2)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Multiplicador de Números</h1>
+      <br /><br />
+      <input type="text" className="text" placeholder="Digite o primeiro n°" onChange={e => setNumber1(e.target.value)}/>
+      <input type="text" className="text" placeholder="Digite o segundo n°" onChange={e => setNumber2(e.target.value)}/>
+      <br />
+      <input type="button" className="btn btn-calcular" value="Calcular" onClick={calc}/>
+      <br /><br /><br />
+      <span className="resultado">Resultado: {result}</span><br />
     </div>
   );
 }
