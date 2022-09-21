0a8b6d1fcb929659f6ef5347e2129fa04fb86f57
antonio-cajueiro-campos <kimeesan5@gmail.com>
2022-09-21 02:15:15 -0300

    1st proof of concept

--- web-react-contadorpessoas/src/App.js
+++ web-react-contadorpessoas/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { React, useState } from 'react'
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increase = () => {
+    setCounter(count => count + 1)
+  }
+  
+  const decrease = () => {
+    setCounter(count => count - 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Contador de pessoas</h1>
+      <span className="count">{counter}</span><br />
+      <input type="button" className="btn btn-plus" value="+" onClick={increase}/>
+      <br />
+      <input type="button" className="btn btn-minus" value="-" onClick={decrease}/>
     </div>
   );
 }
